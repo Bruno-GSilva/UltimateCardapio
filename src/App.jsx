4e093dce7654a89f75802706d5fa0d17e35de408
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-04-24 13:42:39 -0300

    setup colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
+
 export default  function App() {
 
   return (
     <>
+      <Header/>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default  function Header() {
+
+    return (
+        <header>
+            <div className="bg-gold-20">
+
+            </div>
+        </header>
+    )
+  }
